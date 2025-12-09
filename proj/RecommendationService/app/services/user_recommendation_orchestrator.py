aad2c5972c1b03a3e29189dc74654bf30acd3132
Radosław Maksymiuk <maksymiuk.radoslaw@gmail.com>
2025-12-09 20:06:00 +0100

    update requirements + format RecommendationService

--- proj/RecommendationService/app/services/user_recommendation_orchestrator.py
+++ proj/RecommendationService/app/services/user_recommendation_orchestrator.py
@@ -1,23 +1,25 @@
+from app.models import UserRecommendationResponse
 from app.services import ICandidateGeneratorService, IRankerService, IReRankerService
-from app.models import UserRecmmendationResponse
+
 
 class UserRecommendationOrchestrator:
-    def __init__(self, 
-                 candidate_generator: ICandidateGeneratorService,
-                 ranker: IRankerService,
-                 reranker: IReRankerService):
+    def __init__(
+        self,
+        candidate_generator: ICandidateGeneratorService,
+        ranker: IRankerService,
+        reranker: IReRankerService,
+    ):
         self.candidate_generator = candidate_generator
         self.ranker = ranker
         self.reranker = reranker
-    
+
     def get_user_recommendation(self, user_id):
         candidates = self.candidate_generator.generate_candidates(user_id=user_id)
-        ranked_books = self.ranker.rank_candidates([0,0], candidates=candidates)
+        ranked_books = self.ranker.rank_candidates([0, 0], candidates=candidates)
         final_recommendation = self.reranker.rerank(ranked_books)
 
-        result = UserRecmmendationResponse(
-            user_id=user_id,
-            recommended_books=final_recommendation
+        result = UserRecommendationResponse(
+            user_id=user_id, recommended_books=final_recommendation
         )
 
         return result
